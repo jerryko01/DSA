890d39e35f6897a5e946cf21e3f0fd1ee8857487
jerryko01 <jerrytake01@gmail.com>
2023-07-12 16:29:51 -0400

    Added a new version of dynamic fibonacci sequence

--- S15_DynamicProgramming/fibonacciDynamic.js
+++ S15_DynamicProgramming/fibonacciDynamic.js
@@ -8,22 +8,39 @@ function fibonacci(n) {
 // Since there are so many calls to fibonacci() as the index goes
 // higher, this function is not efficient (Currently O(2^n))
 
-function fibonacciDynamic() {
-    let cache = {};
-    return function fib(n) {
-        if (n in cache) {
-            return cache[n]
-        } else {
-            if (n < 2) {
-                return n;
-            } else {
-                cache[n] = fib(n - 1) + fib(n - 2);
-                return cache[n];
-            }
+// function fibonacciDynamic() {
+//     let cache = {};
+//     return function fib(n) {
+//         if (n in cache) {
+//             return cache[n]
+//         } else {
+//             if (n < 2) {
+//                 return n;
+//             } else {
+//                 cache[n] = fib(n - 1) + fib(n - 2);
+//                 return cache[n];
+//             }
+//         }
+//     }
+// }
+
+// const fasterFib = fibonacciDynamic();
+
+// console.log(fasterFib(5))
+
+function fibonacciDynamic(n) {
+    let memo = {};
+    let count = (k) => {
+        if (memo[k] !== undefined) {
+            return memo[k]
         }
+        if (k <= 1) {
+            return k;
+        }
+        memo[k] = count(k - 2) + count(k - 1);
+        return memo[k]
     }
+    return count(n)
 }
 
-const fasterFib = fibonacciDynamic();
-
-console.log(fasterFib(5))
+console.log(fibonacciDynamic(5))
